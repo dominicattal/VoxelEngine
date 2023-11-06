a00bf0ceb1cfca661dfbfdf5569318aa80cb4c6d
da526 <da526@njit.edu>
2023-11-06 08:41:35 -0500

    Shader code

---
+++ headers/Shader.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include "error.h"
+
+class Shader 
+{
+public:
+    unsigned int ID;
+    Shader(const char* vertex_path, const char* fragment_path);
+    void use();
+};
+
+#endif /* SHADER_H */
---
+++ src/Shader.cpp
@@ -0,0 +1,78 @@
+#include "Shader.h"
+
+Shader::Shader(const char* vertex_path, const char* fragment_path)
+{
+    // 1. retrieve the source code from file
+    std::string vertex_code;
+    std::string fragment_code;
+    std::ifstream vertex_file;
+    std::ifstream fragment_file;
+    vertex_file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fragment_file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+    try
+    {
+        vertex_file.open(vertex_path);
+        fragment_file.open(fragment_path);
+        std::stringstream vertex_stream, fragment_stream;
+        vertex_stream << vertex_file.rdbuf();
+        fragment_stream << fragment_file.rdbuf();
+        vertex_file.close();
+        fragment_file.close();
+        vertex_code = vertex_stream.str();
+        fragment_code = fragment_stream.str();
+    }
+    catch (std::ifstream::failure e) 
+    { 
+        throwError(4); 
+    }
+
+    const char* vertex_shader_code = vertex_code.c_str();
+    const char* fragment_shader_code = fragment_code.c_str();
+
+    // 2. compile shaders
+    unsigned int vertex, fragment;
+    int success;
+    char infoLog[512];
+
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vertex_shader_code, NULL);
+    glCompileShader(vertex);
+    glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+        throwError(5, infoLog);
+    }
+
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fragment_shader_code, NULL);
+    glCompileShader(fragment);
+    glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+        throwError(6, infoLog);
+    }
+
+    // shader program
+    ID = glCreateProgram();
+    glAttachShader(ID, vertex);
+    glAttachShader(ID, fragment);
+    glLinkProgram(ID);
+    glGetProgramiv(ID, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(ID, 512, NULL, infoLog);
+        throwError(7, infoLog);
+    }
+
+    // delete already linked shaders
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+}
+
+void Shader::use()
+{
+    glUseProgram(ID);
+}
