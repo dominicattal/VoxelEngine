64102cbbba5b4a5e4c45c86acacd7860a29f8e6f
da526 <da526@njit.edu>
2023-11-06 11:45:53 -0500

    Got rid of VAO and VBO class, over complicated

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,6 @@
 #include <vec/vec4.h>
 #include "error.h"
 #include "Shader.h"
-#include "VAO.h"
-#include "VBO.h"
 
 void processInput(GLFWwindow* window);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -47,13 +45,18 @@ int main()
          0.0f,  0.5f, 0.0f
     };
 
-    VAO vao1;
-    VBO vbo1;
-    unsigned int VBO;
-    //glGenVertexArrays(1, &VAO);
-    //glBindVertexArray(VAO);
-    //glGenBuffers(1, &VBO);
-    //glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    float vertices2[] = {
+        -0.8f, -0.8f, 0.0f,
+        -0.8f, 0.8f, 0.0f,
+         0.8f, 0.8f, 0.0f,
+         0.8f, -0.8f, 0.0f
+    };
+
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -69,7 +72,7 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         shader.use();
-        vao1.bind();
+        glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         updateDeltaTime();
         glfwPollEvents();
