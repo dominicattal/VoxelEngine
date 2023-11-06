3e44d16c5abdc35d3a41a13a9ff9c8aeb1ae36d8
da526 <da526@njit.edu>
2023-11-06 11:04:50 -0500

    Basic vector classes

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 g++ -g -I./include -I./headers -L./lib include/glad_gl.c src/\\*.cpp -lglfw3dll -o prog
 prog
+del prog
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include "error.h"
 #include "Shader.h"
+#include <vec/vec2.h>
+#include <vec/vec3.h>
+#include <vec/vec4.h>
 
 void processInput(GLFWwindow* window);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -35,14 +38,37 @@ int main()
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
 
     Shader shader("shaders/vertex.sl", "shaders/fragment.sl");
+    vec4f test(3, 5.5, 6, 9);
+    std::cout << test << std::endl;
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    
     frame_time = glfwGetTime();
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glEnable(GL_DEPTH_TEST);
 
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         shader.use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         updateDeltaTime();
         glfwPollEvents();
         glfwSwapBuffers(window);
