3bd1bf220bc6aa2c6260b85eb30c107d2c522a36
da526 <da526@njit.edu>
2023-11-06 07:30:33 -0500

    Create window

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,23 @@
 
 int main() 
 {
+    // initialize glfw
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // initalize window
     GLFWwindow* window = glfwCreateWindow(800, 600, "Voxels", NULL, NULL);
-    throwError(1);
+    if (window == NULL)
+        throwError(1);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 1;
 }
